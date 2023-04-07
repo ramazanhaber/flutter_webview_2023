26adcb3faf61edee3c72ede8940affb63d635aa2
Ramazan HABER <ramazanhaberr@gmail.com>
2023-04-08 00:25:07 +0300

    aa

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter_android/webview_flutter_android.dart';
@@ -57,8 +59,23 @@ class _MyAppState extends State<MyApp> {
           },
         ),
       )
-      ..loadRequest(Uri.parse('https://flutter.dev'));
+      ..loadRequest(Uri.parse('https://www.roketnot.com/'));
   }
+
+  late double _currentPosition = 0.0;
+  late double _startPosition = 0.0;
+
+  Future<bool> _onBack() async {
+    var value = await controller.canGoBack();
+    if (value) {
+      controller.goBack();
+      return false;
+    } else {
+      exit(0);
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -67,12 +84,53 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:   Scaffold(
-        appBar: null,
-        body: SafeArea(child: WebViewWidget(controller: controller)),
+      home:   WillPopScope(
+        onWillPop: _onBack,
+        child: Scaffold(
+         body: SafeArea(
+           child: GestureDetector(
+              onHorizontalDragStart: (details) {
+                _startPosition = details.localPosition.dx;
+                print(_startPosition);
+              },
+              onHorizontalDragUpdate: (details) {
+                setState(() {
+                  _currentPosition = details.localPosition.dx;
+                  print(_currentPosition);
+
+                });
+              },
+              onHorizontalDragEnd: (details) async {
+                if (_currentPosition - _startPosition > 50) {
+                  if (await controller.canGoBack()) {
+                    controller.goBack();
+                  }
+                }
+                setState(() {
+                  _currentPosition = 0.0;
+                  _startPosition = 0.0;
+                });
+              },
+              child: Stack(
+                children: [
+                  WebViewWidget(controller: controller),
+                  Positioned(
+                    left: _currentPosition - 50.0,
+                    top: 50.0,
+                    child: Opacity(
+                      opacity: (_currentPosition - _startPosition)==0?1:50 / 50.0,
+                      child: const Icon(Icons.arrow_back),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+         ),
+        ),
       ),
     );
   }
+
 }
 
 
